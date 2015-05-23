5626caa83e35f7ee6b14966d51a528bf42605900
Priit Haamer <priit@fraktal.ee>
2015-05-23 10:41:00 +0300

    Initial Commit

---
+++ Paevapraed/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Paevapraed
+//
+//  Created by Priit Haamer on 23.05.15.
+//  Copyright (c) 2015 Priit Haamer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
