ee7a389b554e9cdf8f58f53134b48683ca0858d1
Priit Haamer <priit@fraktal.ee>
2015-08-10 22:56:44 +0300

    Add hotpot scraper.

---
+++ Scrapers/hotpot.rb
@@ -0,0 +1,95 @@
+require 'json'
+require 'nokogiri'
+require 'date'
+require 'uri'
+require 'net/http'
+
+MonthNames = {
+  jaanuar: 1, veebruar: 2, märts: 3, aprill: 4, mai: 5, juuni: 6,
+  juuli: 7, august: 8, september: 9, oktoober: 10, november: 11, detsember: 12
+}
+
+Prices = {
+  'supp' => 2.50,
+  'hiina menÜÜ' => 3.50,
+  'tai menÜÜ' => 4.50,
+  'india menÜÜ' => 4.50
+}
+
+def parse_hotpot_date(datestr)
+  MonthNames.each do |name, month|
+    datestr.gsub!(%r{#{name.to_s}}i, month.to_s)
+  end
+  
+  datestr += ".#{Time.now.strftime('%Y')}"
+  datestr.gsub!(/\s+/, '')
+  
+  DateTime.strptime(datestr, '%d.%m.%Y')
+end
+
+file = File.open(File.join(File.dirname(__FILE__), 'spec', 'fixtures', 'hotpot.html'))
+
+doc = Nokogiri::HTML(file)
+
+content = doc.css('#main .width-container')
+
+data = {}
+currdate = nil
+price = nil
+name = nil
+description = nil
+
+content.children.each do |item|
+  if item.name == 'h2' || item.name == 'h1'
+    if name && price && description
+      data[currdate][:offers] << {name: name, description: description, price: price}
+      name, price, description = nil, nil, nil
+    end
+    
+    currdate = parse_hotpot_date(item.text).strftime('%Y-%m-%d')
+    
+    data[currdate] = {
+      date: currdate,
+      city_code: 'tartu',
+      country_code: 'ee',
+      currency: 'EUR',
+      restaurant_code: 'hotpot',
+      offers: []
+    }
+  end
+  
+  if Prices.keys.include?(item.text.strip.downcase)
+    price = Prices.fetch(item.text.strip.downcase)
+  end
+  
+  if item.name == 'p' && item.css('> strong').length > 0
+    name = item.children[0].text
+    
+    if item.children.length > 1
+      description = item.children[1].text
+    end
+  elsif name && description == nil && item.text.length > 1
+    description = item.text
+  end
+  
+  if name && price && description
+    data[currdate][:offers] << {name: name, description: description, price: price}
+    name, price, description = nil, nil, nil
+  end
+end
+
+data.values.each do |day|
+  puts '-----------------'
+  puts day.to_json
+  
+  # uri = URI('http://voog.construction:3000/api/v1/offers')
+  # req = Net::HTTP::Post.new(uri)
+  # req.body = day.to_json
+  # 
+  # res = Net::HTTP.start(uri.hostname, uri.port) do |http|
+  #   http.request(req)
+  # end
+
+  # puts res.code
+  # puts res.body
+end
